8c448d4604bd7768f4bcf253a575e70d0427dd65
Gary Kim <gary@garykim.dev>
2020-12-10 23:53:29 -0500

    enh: pull all programs
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- cooper/website.go
+++ cooper/website.go
@@ -35,7 +35,20 @@ type CourseInfo struct {
 }
 
 func ScrapeInfo () ([]CourseInfo, error) {
-	res, err := http.Get("https://cooper.edu/engineering/curriculum/courses")
+	var tr []CourseInfo
+	urls := []string{"https://cooper.edu/engineering/curriculum/courses", "https://cooper.edu/humanities/curriculum/courses", "https://cooper.edu/art/curriculum/courses", "https://cooper.edu/architecture/curriculum/courses"}
+	for _, url := range urls {
+		t, err := scrapePage(url)
+		if err != nil {
+			return nil, err
+		}
+		tr = append(tr, t...)
+	}
+	return tr, nil
+}
+
+func scrapePage(url string) ([]CourseInfo, error) {
+	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -52,13 +65,15 @@ func ScrapeInfo () ([]CourseInfo, error) {
 	var tr []CourseInfo
 
 	doc.Find("#course-listings .content li").Each(func(i int, selection *goquery.Selection) {
-		description := selection.Find("p").First().Text()
-		tr = append(tr, CourseInfo{
-			Code:        strings.ToUpper(strings.TrimSpace(selection.Find("h3").Text())),
-			Name:        strings.TrimSpace(selection.Find("h4").Text()),
-			Description: strings.TrimSpace(description),
-			ExtraInfo:  strings.TrimSpace(strings.TrimPrefix(selection.Find("p").Text(), description)),
-		})
+		for _, courseCode := range strings.Split(selection.Find("h3").Text(), ",") {
+			description := selection.Find("p").First().Text()
+			tr = append(tr, CourseInfo{
+				Code:        strings.ToUpper(strings.TrimSpace(courseCode)),
+				Name:        strings.TrimSpace(selection.Find("h4").Text()),
+				Description: strings.TrimSpace(description),
+				ExtraInfo:  strings.TrimSpace(strings.TrimPrefix(selection.Find("p").Text(), description)),
+			})
+		}
 	})
 	return tr, nil
 }
